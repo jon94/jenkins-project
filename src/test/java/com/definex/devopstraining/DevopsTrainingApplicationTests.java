5caa4166126299ac3f2eadd635848e259f7f0085
jon94 <jonathan.lim@datadoghq.com>
2023-03-18 17:42:31 +0800

    clean up repo

---
+++ src/test/java/com/definex/devopstraining/DevopsTrainingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.definex.devopstraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsTrainingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
